60d977da84700b926efcc8cd2682433d4517a1a8
huycozy <huy@nevercode.io>
2026-04-16 19:04:14 +0700

    shutdown wishlist tmp

--- assets/store-links.js
+++ assets/store-links.js
@@ -1,6 +1,6 @@
 const COMPACTKIT_STORE_URLS = {
-  appStore: "https://www.apple.com/app-store/",
-  playStore: "https://play.google.com/store",
+  appStore: "https://apps.apple.com/vn/app/id6761741587",
+  playStore: "https://play.google.com/store/apps/details?id=com.app.compactkit",
 };
 
 window.COMPACTKIT_STORE_URLS = COMPACTKIT_STORE_URLS;
